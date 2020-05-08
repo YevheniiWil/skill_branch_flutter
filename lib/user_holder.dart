906f0ca751c2c99c8ef9a23bcaafe333e893bce2
yevheniia <yevhenii.antipenko@actionmediamtl.com>
2020-05-08 19:07:42 +0300

    2 les

---
+++ lib/user_holder.dart
@@ -0,0 +1,19 @@
+import 'package:skill_branch_flutter/models/user.dart';
+
+import 'string_utils.dart';
+
+class UserHolder {
+  Map<String, User> users = {};
+
+  void registerUser(String name, String phone, String email) {
+    User user = User(name: name, phone: phone, email: email);
+
+    print(user.toString());
+
+    if(!users.containsKey(user.login)){
+      users[user.login] = user;
+    } else {
+      throw Exception ('A user with this name alredy exists');
+    }
+  }
+}
